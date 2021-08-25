9fab3f0b2b87c92da355bf23d3241c5327c5e8c7
Harut <manukyanharut1994@gmail.com>
2021-08-25 15:26:36 +0400

    last3

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = express.env.port || 8083;
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('')
